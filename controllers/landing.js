68991cf9d9cfb0d225f75adfb35bd4a69ba927c6
sundaycrunk <ishvara156@gmail.com>
2021-09-25 16:13:02 +1000

    .

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,3 +1,10 @@
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Express' });
 }
+// ok brilliant.
+// the console is the dev console. not chrometools.
+// it appears in the terminal.
+exports.submit_lead = function(req, res, next) {
+  console.log("lead email:", req.body.lead_email)
+  res.redirect('/')
+}
